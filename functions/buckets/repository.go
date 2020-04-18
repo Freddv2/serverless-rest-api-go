f39f4155dfd07fc0b0915b506b8afe5a04c52d45
Freddv2 <fredddv@gmail.com>
2020-04-18 19:10:11 -0400

    Implemented Get bucket by name repository.go

--- functions/buckets/repository.go
+++ functions/buckets/repository.go
@@ -2,6 +2,7 @@ package buckets
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
@@ -14,7 +15,7 @@ type DynamoDBRepository struct {
 func (r *DynamoDBRepository) Get(ctx context.Context, tenantId string, bucketId string) (*Bucket, error) {
 	bucket := &Bucket{}
 	input := &dynamodb.GetItemInput{
-		TableName: &r.tableName,
+		TableName: aws.String(r.tableName),
 		Key: map[string]*dynamodb.AttributeValue{
 			"tenantId": {
 				S: &tenantId,
@@ -33,6 +34,46 @@ func (r *DynamoDBRepository) Get(ctx context.Context, tenantId string, bucketId 
 	err = dynamodbattribute.UnmarshalMap(result.Item, bucket)
 	if err != nil {
 		return nil, err
+	}
+
+	return bucket, nil
+}
+
+func (r *DynamoDBRepository) GetByName(ctx context.Context, tenantId string, bucketName string) (*Bucket, error) {
+	bucket := &Bucket{}
+	input := &dynamodb.QueryInput{
+		TableName: aws.String(r.tableName),
+		KeyConditions: map[string]*dynamodb.Condition{
+			"tenantId": {
+				ComparisonOperator: aws.String("EQ"),
+				AttributeValueList: []*dynamodb.AttributeValue{
+					{
+						S: aws.String(tenantId),
+					},
+				},
+			},
+		},
+		ExpressionAttributeNames: map[string]*string{
+			":name": aws.String("name"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":bucketName": {
+				S: aws.String(bucketName),
+			},
+		},
+		FilterExpression: aws.String(":name = :bucketName"),
+		Limit:            aws.Int64(1),
+	}
+	result, err := r.session.QueryWithContext(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(result.Items) > 0 {
+		err = dynamodbattribute.UnmarshalMap(result.Items[0], bucket)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return bucket, nil
