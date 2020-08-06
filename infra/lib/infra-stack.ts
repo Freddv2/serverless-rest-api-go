5a746ef9669feb6ee5aa058ae578ba2422864753
fvincent <fvincent@okiok.com>
2020-08-06 11:18:55 -0400

    Created IaC with CDK

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import {Code, Function, Runtime} from '@aws-cdk/aws-lambda';
+import {LambdaRestApi} from "@aws-cdk/aws-apigateway";
+import {AttributeType, Table} from "@aws-cdk/aws-dynamodb";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambda = new Function(this, 'Bucket-Lambda', {
+      runtime: Runtime.GO_1_X,
+      code: Code.fromAsset("../functions/buckets/build/go_build_main_go_linux.zip"),
+      handler: 'main',
+      memorySize: 128,
+    });
+    new LambdaRestApi(this, id + 'Bucket-API', {
+      handler: lambda,
+    });
+    const table = new Table(this, 'Bucket-Table', {
+      tableName: 'BUCKET',
+      partitionKey: {name: 'tenantId', type: AttributeType.STRING},
+      sortKey: {name: 'bucketId', type: AttributeType.STRING},
+      readCapacity: 1,
+      writeCapacity: 1
+    });
+
+    table.grantReadWriteData(lambda)
   }
 }
